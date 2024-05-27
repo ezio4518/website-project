24edb19ad3379afba3ce2a6bb797fb3326582237
ezio4518 <mdgufran4518@gmail.com>
2024-05-28 03:28:05 +0530

    footer completed but css left

--- frontend/src/Components/NewsLetter/NewsLetter.jsx
+++ frontend/src/Components/NewsLetter/NewsLetter.jsx
@@ -4,7 +4,12 @@ import './NewsLetter.css'
 const NewsLetter = () => {
   return (
     <div className='newsletter'>
-        
+        <h1>Get Exclusive Offers On Your Email</h1>
+        <p>Subscribe to our newsletter and stay updated</p>
+        <div>
+            <input type="email" placeholder='Your Email id' />
+            <button>Subscribe</button>
+        </div>
     </div>
   )
 }
